43c124b9d8e80ee385cb225c9e26299c9c9dcdba
Medhat Elmasry <medhat@elmasry.ca>
2020-06-24 12:37:28 -0700

    updated constants.ts

--- api/constants.ts
+++ api/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
      public static config = {
         endpoint: "ENDPOINT-HERE",
-        key: "COSMOS-DB-HERE",
+        key: "COSMOS-DB-KEY-HERE",
         databaseId: "Inventory",
         containerId: "Products",
         partitionKey: { kind: "Hash", paths: ["/brand/name"] }
